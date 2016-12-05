57a6aa2af44360b8af48ce94d872d6d9f1e77c8d
geiziry <mahmoud.geiziry@gmail.com>
2016-12-05 15:02:39 +0300

    adjusted the DashboradItem border dropshadow and the spacing between the items

--- Fdp.Controls/ViewModels/DashboardViewModel.cs
+++ Fdp.Controls/ViewModels/DashboardViewModel.cs
@@ -1,9 +1,7 @@
 ﻿using DevExpress.Mvvm;
 using Fdp.Controls.CommonTypes;
 using System.Collections.ObjectModel;
-using System.Windows.Input;
 using System;
-using System.Reflection;
 
 namespace Fdp.Controls.ViewModels
 {
@@ -44,27 +42,6 @@ namespace Fdp.Controls.ViewModels
             }
         }
 
-        private DelegateCommand<string> _AddItemCommand;
-        public ICommand AddItemCommand
-        {
-            get
-            {
-                return _AddItemCommand ?? (_AddItemCommand = new DelegateCommand<string>(s =>
-                 {
-                     switch (s)
-                     {
-                         case "LogView":
-                             Items.Add(new LogViewModel { Label = "Log" });
-                             break;
-                         case "PlotView":
-                             Items.Add(new PlotViewModel { Label = "Plot" });
-                             break;
-                         default:
-                             break;
-                     }
-                 }));
-            }
-        }
 
 
     }
